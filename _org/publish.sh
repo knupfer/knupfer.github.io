f7e1d8ee9560f4c24194e31fd3667f32e944789f
Knupfer <fknupfer@gmail.com>
2013-10-30 11:26:43 +0100

    skriptupdate

--- _org/publish.sh
+++ _org/publish.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+TEMP='../_processing';
+TOC='totalindex';
+
 echo '---
 layout: default
 title: Index
@@ -7,33 +10,31 @@ title: Index
 <div id="table-of-contents2">
 <h3>Index</h3>
 <div id="text-table-of-contents2">
-<ul>' > ../_processing/totalindex;
+<ul>' > $TEMP/$TOC;
 
 for FILE in *.org
 do
-    if [ $FILE != 'publish.org' ]
-    then
-        DATEI=$(echo $FILE | sed 's_\(.*\).org_\1_');
-        URL=$(echo /$DATEI | sed 's_-_/_; s_-_/_; s_-_/_; s_$_.html_')
-        TITLE=$(sed -n '2,/---/ s_title: *"*\([^"]*\)"*_\1_p' $DATEI.org);
-        FATHER=$(sed -n '2,/---/ s_father: *"*\([^"]*\)"*_\1_p' $DATEI.org);
-        
-        test -e ../_processing/$DATEI.html &&
-        
-        echo '<li><a href="'$URL'">'$TITLE'</a>' >> categorie.$FATHER &&
-        sed -n '/<div id="text-table-of-contents">/,/<\/div>/ p' ../_processing/$DATEI.html | 
-            tail -n +2 | 
-            head -n -1 | 
-        sed 's:\(href="\)#:\1'$URL'#:g' >> categorie.$FATHER &&
-        sed -n '2,/---/ p' $DATEI.org > ../_processing/$DATEI.org.publish &&
-        sed 'N;
-            s_[(</ul>)(</dl>)]\n</div>_&<p></p>_;
-            P;
-            s_file:///_/_;
-            s_<h2>Table of Contents</h2>_<h3>Inhaltsverzeichnis</h3>_;
-            D' ../_processing/$DATEI.html >> ../_processing/$DATEI.org.publish &&
-        cat ../_processing/$DATEI.org.publish > ../_posts/$DATEI.html;
-    fi
+    DATEI=$(echo $FILE | sed 's_\(.*\).org_\1_');
+    URL=$(echo /$DATEI | sed 's_-_/_; s_-_/_; s_-_/_; s_$_.html_')
+    TITLE=$(sed -n '2,/---/ s_title: *"*\([^"]*\)"*_\1_p' $DATEI.org);
+    FATHER=$(sed -n '2,/---/ s_father: *"*\([^"]*\)"*_\1_p' $DATEI.org);
+
+    
+    test -e $TEMP/$DATEI.html &&
+    
+    echo '<li><a href="'$URL'">'$TITLE'</a>' >> categorie.$FATHER &&
+    sed -n '/<div id="text-table-of-contents">/,/<\/div>/ p' $TEMP/$DATEI.html | 
+        tail -n +2 | 
+        head -n -1 | 
+    sed 's:\(href="\)#:\1'$URL'#:g' >> categorie.$FATHER &&
+    sed -n '2,/---/ p' $DATEI.org > $TEMP/$DATEI.org.publish &&
+    sed 'N;
+        s_[(</ul>)(</dl>)]\n</div>_&<p></p>_;
+        P;
+        s_file:///_/_;
+        s_<h2>Table of Contents</h2>_<h3>Inhaltsverzeichnis</h3>_;
+        D' $TEMP/$DATEI.html >> $TEMP/$DATEI.org.publish &&
+    cat $TEMP/$DATEI.org.publish > ../_posts/$DATEI.html;
 done
 
 mv categorie. categorie.ZZZ
@@ -43,16 +44,16 @@ do
     if [ "$(echo $INDEX | sed 's_categorie\.\(.*\)_\1_')" != 'ZZZ' ]
     then
         echo '<li><a href="">' $(echo $INDEX | 
-            sed 's_categorie\.\(.*\)_\1_') '</a>' '<ul>' >> ../_processing/totalindex;
+            sed 's_categorie\.\(.*\)_\1_') '</a>' '<ul>' >> $TEMP/$TOC;
     else
-        echo '<li><a href="">' Verschiedenes '</a>' '<ul>' >> ../_processing/totalindex;
+        echo '<li><a href="">' Verschiedenes '</a>' '<ul>' >> $TEMP/$TOC;
     fi
-    cat $INDEX >> ../_processing/totalindex;
-    echo '</ul>' >> ../_processing/totalindex;
+    cat $INDEX >> $TEMP/$TOC;
+    echo '</ul>' >> $TEMP/$TOC;
 done
 
-echo '</ul></div></div>' >> ../_processing/totalindex;
-cp ../_processing/totalindex  ../totalindex.html;
-mv categorie.* ../_processing/;
+echo '</ul></div></div>' >> $TEMP/$TOC;
+cp $TEMP/$TOC  ../$TOC.html;
+mv categorie.* $TEMP;
 cp ../_posts/about.html ../about.html;
 test -e *.org~ && rm *.org~ ;
